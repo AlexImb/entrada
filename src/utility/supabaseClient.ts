a7622d890ade39a73c9bf61edccd34133104801c
Alex Imbrea <alex@elnino.tech>
2023-11-08 17:26:25 +0100

    Scaffold Refine app

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://hiepmlulwtrdfupomzuw.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZXBtbHVsd3RyZGZ1cG9tenV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk0NTkwNDAsImV4cCI6MjAxNTAzNTA0MH0.bQ-8ThMXbOCHjej63BEnXiiqqFqSHQBiBddOlH2VKgg";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
